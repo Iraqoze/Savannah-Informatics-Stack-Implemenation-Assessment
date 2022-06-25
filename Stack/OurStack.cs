56e555afe6046ec14b1228346bf2822d414d1881
Lionel Irakoze <iraqoze.lionel@gmail.com>
2022-06-25 09:51:54 +0300

    Stack Implementation

---
+++ Stack/OurStack.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurStack
+{
+
+    #region  This is a generic class that contains items of the specified type
+        //T represent our generic type
+        //T Value is the generic value
+        //Count is to keep track of the number of items
+    #endregion
+
+    public class OurStack<T>
+    {
+        private T[] Items;
+        private T? Value;
+        private int Count;
+        public OurStack(int Length)
+        {
+            Items = new T[Length];
+        }
+
+
+        //Push
+        public void Push(T value)
+        {
+            if (Count == Items.Length)
+                throw new Exception("Stack Overflow");
+            Items[Count++] = value;
+
+        }
+        //Peek
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new Exception("Empty Stack");
+            return Items[Count - 1];
+        }
+
+        //Pop
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new Exception("Empty Stack");
+
+            var item = Items[Count - 1];
+
+            Items[Count - 1] = Value;
+            Count--;
+            return item;
+        }
+
+        //Is Empty
+        public bool IsEmpty() => Count == 0;        
+
+        //Count
+        public int GetCount() => Count;
+
+        //Contains
+        public bool Contains(T value)
+        {
+            if(IsEmpty())
+                return false;
+            for (int i = 0; i < Count; i++)
+            {
+                if (value.Equals(Items[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        //Clear Stack
+        public void Clear()
+        {
+            Count = 0;
+            Items = null;
+        }
+        //Reverse the Stack
+        public void Reverse()
+        {
+            var reversedStack = new T[Count];
+            var index = 0;
+            for (int i = Count-1; i >= 0; i--)
+                reversedStack[index++] = Items[i];
+
+            for (int i = 0; i < reversedStack.Length; i++)
+                Items[i]=reversedStack[i];
+                          
+        }
+
+        //To String
+        public override string? ToString()
+        {
+            if (IsEmpty())
+                return "";
+            var sb = new StringBuilder();
+            for (int i = 0; i < Count; i++)
+                sb.Append($"{Items[i]}, ");
+            
+            return sb.ToString();
+        }
+
+        
+
+    }
+}
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Testing our Stack
+
+using OurStack;
+
+var ourSatck = new OurStack<string>(5);
+
+// We have The following Items:  A,B,C,D,E
+
+//Operation 1: Pushing the items onto the stack
+
+ourSatck.Push("A");
+ourSatck.Push("B");
+ourSatck.Push("C");
+ourSatck.Push("D");
+ourSatck.Push("E");
+
+Console.WriteLine($"\nPrinting Pushed Items:\n{ourSatck}");
+
+//Operation 2: Peek
+Console.WriteLine($"\nPeeking Item:\n{ourSatck.Peek()}");
+
+//Operation 3: Pop
+Console.WriteLine($"\nPopping Item:\n{ourSatck.Pop()}");
+Console.WriteLine($"\nPrinting Items After Pop Operation:\n{ourSatck}");
+
+//Operation 4: Count
+Console.WriteLine($"\nNumber of Items:\n{ourSatck.GetCount()}");
+
+
+//Operation 5: Contains
+Console.WriteLine($"\nCheck whether the stack contains a given item: for instance 'C'\n{ourSatck.Contains("C")}");
+
+//operation 6: Reverse Stack
+ourSatck.Reverse();
+Console.WriteLine($"\nReversed Stack:\n{ourSatck}");
+
+//operation 7: Checking whether the stack is empty
+Console.WriteLine($"\nChecking If stack is empty:\n{ourSatck.IsEmpty()}");
+
+//operation 8: Clearing the stack
+ourSatck.Clear();
+Console.WriteLine($"\nAfter Clearing, The stack is empty:\n{ourSatck.GetCount()}");
+
+
+
+
+
+Console.ReadLine();
